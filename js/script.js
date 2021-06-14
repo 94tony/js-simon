0ae0e4167ab28e0225857b39f538a1edb5c8d2b0
94tony <dagatatony94@gmail.com>
2021-06-14 17:44:44 +0200

    struttura 2

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ in questo caso creare un array con i numeri generati  []
 
 var max = 100
 var min = 1
+
+var numberRemind = []
+
+
 function  Generatore(min,max){
 var Result =  Math.floor(Math.random() *(max-min+1)) +min 
 return Result;
@@ -35,16 +39,41 @@ far partire un timer di 30 secondi   setTimeout(nomeFunzione, tempoDaAspettare);
   3 step  
    dopo i 30 secondi creare 5 prompt  dove  inserire i numeri visti (for o while  ?)  
 */
+
+
 setTimeout(timer,3000);
 function timer (){
-    var chooseNumb  = prompt("scegli il numero") ;
-    return chooseNumb
+    
+    var ArrayUtente = []
+    var numberRemind = []
+    var flag = false
+
+    while(ArrayUtente.length < 5 ){
+  
+    var chooseNumb  = parseInt( prompt("scegli il numero")) ;
+    ArrayUtente.push(chooseNumb)
+
+    }
+
+ for(var i = 0; i<5 ; i++ ){
+     if(ArrayAllert.includes(ArrayUtente[i])){
+         flag=true;
+         if(flag){
+             numberRemind.push(ArrayUtente[i])
+         };
+    
+     }else{
+         flag =false
+     }
+ }
+ alert(" il tuo punteggio è : " + numberRemind.length + " " +"i numeri indovinati sono  " + numberRemind)
 }
 
 
 
 
-var ArrayUtente = []
+
+
 
 /*
 4 step
